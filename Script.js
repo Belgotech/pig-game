71e65fde136cfaae83a97e6a5d9b55275a09f079
Belgotech <Belovedobiora2002@gmail.com>
2022-10-20 08:15:56 -0700

    working

--- Script.js
+++ Script.js
@@ -1,8 +1,9 @@
 // var dice = math.floor(math.random() * 6) + 1;
+var dice1 = Math.floor(Math.random() * 6) + 1;
 // var dice = math.random() * 6;
 
 document.querySelector(".btn-roll").addEventListener("click", function () {
   diceDOM = document.querySelector(".dice");
   diceDOM.style.display = "block";
-  diceDOM.src = "dice-"  + ".png";
+  diceDOM.src = "dice" + dice1 + ".png";
 });
