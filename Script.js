9fc9bb932eabb0ec0e0285c53e4349de9245a64d
Belgotech <Belovedobiora2002@gmail.com>
2022-10-21 19:35:47 -0700

    changes

--- Script.js
+++ Script.js
@@ -26,12 +26,32 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     roundScore += dice
     document.querySelector('#current-' + activePlayer).textContent = roundScore
   }else{
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0
+    nextPlayer()
   }
 })
 
+function nextPlayer(){
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
 
+    document.querySelector(".player-0-panel").classList.toggle("active")
+    document.querySelector(".player-1-panel").classList.toggle("active")
+    // document.querySelector(".player-1-panel").classList.remove(".active")
+    // document.querySelector(".player-1-panel").classList.add(".active")
+
+    document.querySelector(".dice").style.display = "none"
+}
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+  //add CURRENT scores to GLOBAL score
+  scores[activePlayer] += roundScore
+  //update the UI
+  document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer] 
+  //check if player won
+  nextPlayer()
+})
 
 
 
